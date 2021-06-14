bf89bfe5b04e5bd314dc10a3911cd2c2138b4689
khashimakhun <55402552+khashimakhun@users.noreply.github.com>
2021-06-14 11:31:52 +0600

    Delete src/main/java/com/example/medcenter/repoitory directory

--- src/main/java/com/example/medcenter/repoitory/DoctorsTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.medcenter.repoitory;
-
-import com.example.medcenter.entity.DoctorsTypeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DoctorsTypeRepository extends JpaRepository<DoctorsTypeEntity,Integer> {
-}
